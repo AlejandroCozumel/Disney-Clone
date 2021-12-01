691b1740d2e5f2f5b8b3bb234e863cc812a84d2f
Alejandro Zamora <alejandroo14@gmail.com>
2021-12-01 00:52:19 -0500

    chequeo de errores

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,16 +1,16 @@
 import { getSession, useSession } from "next-auth/client";
-import { useEffect, useState } from "react";
-import { PlusIcon, XIcon } from "@heroicons/react/solid";
-import { useRouter } from "next/router";
 import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
-import Image from "next/image";
-import ReactPlayer from 'react-player/lazy'
+import { PlusIcon, XIcon } from "@heroicons/react/solid";
+import ReactPlayer from "react-player/lazy";
 
 function Movie({ result }) {
-  const BASE_URL = "https://image.tmdb.org/t/p/original/";
   const [session] = useSession();
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
   const router = useRouter();
   const [showPlayer, setShowPlayer] = useState(false);
 
@@ -24,9 +24,8 @@ function Movie({ result }) {
     (element) => element.type === "Trailer"
   );
 
-  console.log(result);
   return (
-    <div>
+    <div className="relative">
       <Head>
         <title>{result.title || result.original_name}</title>
         <link rel="icon" href="/favicon.ico" />
@@ -47,16 +46,16 @@ function Movie({ result }) {
             />
           </div>
           <div className="absolute inset-y-28 md:inset-y-auto md:bottom-10 inset-x-4 md:inset-x-12 space-y-6 z-50">
-            <h1 className="text-3xl sm:text-4xl m:text-5xl">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
               {result.title || result.original_name}
             </h1>
             <div className="flex items-center space-x-3 md:space-x-5">
               <button className="text-xs md:text-base bg-[#f9f9f9] text-black flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]">
                 <img
                   src="/images/play-icon-black.svg"
-                  alt="Play"
+                  alt=""
                   className="h-6 md:h-8"
-                ></img>
+                />
                 <span className="uppercase font-medium tracking-wide">
                   Play
                 </span>
@@ -68,20 +67,20 @@ function Movie({ result }) {
               >
                 <img
                   src="/images/play-icon-white.svg"
-                  alt="Trailer"
+                  alt=""
                   className="h-6 md:h-8"
-                ></img>
+                />
                 <span className="uppercase font-medium tracking-wide">
-                  Play Trailer
+                  Trailer
                 </span>
               </button>
 
-              <div className="rounded-full border-2 border-white cursor-pointer flex items-center justify-center w-11 h-11 bg-black/60">
+              <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
                 <PlusIcon className="h-6" />
               </div>
 
-              <div className="rounded-full border-2 border-white cursor-pointer flex items-center justify-center w-11 h-11 bg-black/60">
-                <img src="/images/group-icon.svg" alt="Group Button" />
+              <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                <img src="/images/group-icon.svg" alt="" />
               </div>
             </div>
 
@@ -93,8 +92,9 @@ function Movie({ result }) {
             <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
           </div>
 
+          {/* Bg Overlay */}
           {showPlayer && (
-            <div onClick={() => setShowPlayer(false)} className="absolute inset-y-0 bg-black h-full w-full opacity-50 z-50"></div>
+            <div className="absolute inset-0 bg-black opacity-50 h-full w-full z-50"></div>
           )}
 
           <div
@@ -132,11 +132,10 @@ export default Movie;
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
-
   const { id } = context.query;
 
   const request = await fetch(
-    `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
   ).then((response) => response.json());
 
   return {
