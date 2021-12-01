05fc1a53cdfe02f3aa20aa9db6d9b37a445ea9be
Alejandro Zamora <alejandroo14@gmail.com>
2021-12-01 16:32:24 -0500

    añadida ruta de shows de tv

--- pages/show/[id].js
+++ pages/show/[id].js
@@ -136,7 +136,7 @@ export async function getServerSideProps(context) {
   const { id } = context.query;
 
   const request = await fetch(
-    `https://api.themoviedb.org/3/show/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
+    `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
   ).then((response) => response.json());
 
   return {
